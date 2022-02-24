2851664a761379c13e8a03016c7766d31f11b8af
carlie-gorrondona <carlie.gorrondona@gmail.com>
2022-02-24 13:49:07 -0600

    added app.use(express.static....

--- server/index.js
+++ server/index.js
@@ -15,6 +15,8 @@ rollbar.log('Hello world!')
 const app = express();
 app.use(express.json());
 
+app.use(express.static(path.join(__dirname, '../public')))
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'))
 });
